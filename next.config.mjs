6b8c4285cd8f46129ae57f5bed4f3891422c3dc2
Sajid al-Islam <saajiidi@gmail.com>
2024-06-05 16:02:04 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
-import next from "next";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
