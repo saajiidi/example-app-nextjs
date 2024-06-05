820f9070e05038f92db1fb3b8216af0e0f9ad030
Sajid al-Islam <saajiidi@gmail.com>
2024-06-05 19:20:18 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /**  basePath: "/example-app-nextjs",
-   * reactStrictMode: true,
-   */
+  basePath: "/example-app-nextjs",
+  reactStrictMode: true,
+
   output: "export",
 
   images: {
