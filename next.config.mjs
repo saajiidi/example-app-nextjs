f5d1d79c3190ebbd3e21dae28bd0256a9507731b
Sajid al-Islam <saajiidi@gmail.com>
2024-06-05 19:15:29 +0600

    new

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  /**  basePath: "/example-app-nextjs",
+   * reactStrictMode: true,
+   */
   output: "export",
+
   images: {
     unoptimized: true,
   },
